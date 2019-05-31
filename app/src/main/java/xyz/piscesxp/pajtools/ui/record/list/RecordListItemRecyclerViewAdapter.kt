ecf44e3aeb3b22af6908eec4bf68077240e2071e
Zhenxi <PiscesXP@outlook.com>
2019-05-31 21:38:52 +0800

    添加menu方块.(逻辑未完成)

--- app/src/main/java/xyz/piscesxp/pajtools/ui/record/list/RecordListItemRecyclerViewAdapter.kt
+++ app/src/main/java/xyz/piscesxp/pajtools/ui/record/list/RecordListItemRecyclerViewAdapter.kt
@@ -11,8 +11,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_record_list_item.view.*
-import org.w3c.dom.Text
-
+import kotlinx.android.synthetic.main.record_menu.view.*
 
 import xyz.piscesxp.pajtools.ui.record.list.RecordListFragment.RecordListFragmentListeners
 import xyz.piscesxp.pajtools.dummy.DummyContent.DummyItem
@@ -22,18 +21,18 @@ import xyz.piscesxp.pajtools.data.record.RecordData
 import java.text.SimpleDateFormat
 import java.util.*
 
-/**
- * [RecyclerView.Adapter] that can display a [DummyItem] and makes a call to the
- * specified [RecordListFragmentListeners].
- * TODO: Replace the implementation with code for your data type.
- */
 class RecordListItemRecyclerViewAdapter(
     private val mValues: List<RecordData>,
     private val mListenerRecord: RecordListFragmentListeners?
-) : RecyclerView.Adapter<RecordListItemRecyclerViewAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val mOnClickListener: View.OnClickListener
     private val TAG = this.javaClass.canonicalName
+
+    enum class VIEW_TYPE(val type: Int) {
+        ITEM_TYPE_MENU(0),
+        ITEM_TYPE_RECORD(1)
+    }
 
     init {
         mOnClickListener = View.OnClickListener { v ->
@@ -44,79 +43,118 @@ class RecordListItemRecyclerViewAdapter(
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_record_list_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = mValues[position]
-        //日期
-        val date = Date((item.endTime).toLong() * 1000)
-        holder.mMatchTime.text = SimpleDateFormat("yyyy-MM-dd kk:mm").format(date)
-        //模式
-        holder.mMatchType.text = when (item.matchID) {
-            10 -> "资质赛"
-            21 -> "逢魔之战"
-            2 -> "为崽而战"
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            VIEW_TYPE.ITEM_TYPE_MENU.type ->
+                return MenuViewHolder(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.record_menu, parent, false)
+                )
+            VIEW_TYPE.ITEM_TYPE_RECORD.type ->
+                return ItemViewHolder(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.fragment_record_list_item, parent, false)
+                )
             else -> {
-                Log.d(TAG, "Unknown match type ${item.matchID}")
-                "未知模式"
+                return ItemViewHolder(
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.fragment_record_list_item, parent, false)
+                )
             }
         }
-        //备份
-        val backup = true
-        if (backup) {
-            holder.let {
-                it.mBackupButton.visibility = View.INVISIBLE
-                it.mSuccessPrompt.visibility = View.VISIBLE
-                it.mStateIcon.visibility = View.VISIBLE
-            }
-        } else {
-            holder.let {
-                it.mBackupButton.visibility = View.VISIBLE
-                it.mSuccessPrompt.visibility = View.INVISIBLE
-                it.mStateIcon.visibility = View.INVISIBLE
-                it.mBackupButton.setOnClickListener { v ->
-                    holder.mSuccessPrompt.text = "备份成功"
-                    mListenerRecord?.onBackupButtonPressed(item)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> VIEW_TYPE.ITEM_TYPE_MENU.type
+            else -> VIEW_TYPE.ITEM_TYPE_RECORD.type
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is MenuViewHolder) {
+            //menu
+            holder.switchAccount.setOnClickListener(View.OnClickListener { holder.switchAccount.text = "ok" })
+
+        } else if (holder is ItemViewHolder) {
+            //record item
+            val item = mValues[position - 1]
+            //日期
+            val date = Date((item.endTime).toLong() * 1000)
+            holder.matchTime.text = SimpleDateFormat("yyyy-MM-dd kk:mm").format(date)
+            //模式
+            holder.matchType.text = when (item.matchID) {
+                10 -> "资质赛"
+                21 -> "逢魔之战"
+                2 -> "为崽而战"
+                else -> {
+                    Log.d(TAG, "Unknown match type ${item.matchID}")
+                    "未知模式"
                 }
             }
-        }
-        //获胜
-        var victory: String
-        var victoryTextColor: Int
-        if (item.result) {
-            victory = "胜利 "
-            victoryTextColor = Color.CYAN
-        } else {
-            victory = "失败 "
-            victoryTextColor = Color.RED
-        }
-        holder.mVictory.let {
-            it.text = victory
-            it.setTextColor(victoryTextColor)
-        }
+            //备份
+            val backup = true
+            if (backup) {
+                holder.let {
+                    it.backupButton.visibility = View.INVISIBLE
+                    it.successPrompt.visibility = View.VISIBLE
+                    it.stateIcon.visibility = View.VISIBLE
+                }
+            } else {
+                holder.let {
+                    it.backupButton.visibility = View.VISIBLE
+                    it.successPrompt.visibility = View.INVISIBLE
+                    it.stateIcon.visibility = View.INVISIBLE
+                    it.backupButton.setOnClickListener { v ->
+                        holder.successPrompt.text = "备份成功"
+                        mListenerRecord?.onBackupButtonPressed(item)
+                    }
+                }
+            }
+            //获胜
+            var victory: String
+            var victoryTextColor: Int
+            if (item.result) {
+                victory = "胜利 "
+                victoryTextColor = Color.CYAN
+            } else {
+                victory = "失败 "
+                victoryTextColor = Color.RED
+            }
+            holder.victory.let {
+                it.text = victory
+                it.setTextColor(victoryTextColor)
+            }
 
-        //加载式神图片
-        val heroID: Int? = item.heroData[item.pos.toString()]?.heroID
-        if (heroID != null) {
-            val url = mListenerRecord?.onLoadHeroImage(heroID)
-            Log.d(TAG, "loading image $url")
-            Picasso.get().load(url).into(holder.mHeroImage)
+            //加载式神图片
+            val heroID: Int? = item.heroData[item.pos.toString()]?.heroID
+            if (heroID != null) {
+                val url = mListenerRecord?.onLoadHeroImage(heroID)
+                Log.d(TAG, "loading image $url")
+                Picasso.get().load(url).into(holder.heroImage)
+            }
         }
     }
 
-    override fun getItemCount(): Int = mValues.size
+    override fun getItemCount(): Int = mValues.size + 1
 
-    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
-        val mMatchType: TextView = mView.matchType
-        val mMatchTime: TextView = mView.matchTime
-        val mBackupButton: Button = mView.backupButton
-        val mSuccessPrompt: TextView = mView.successPrompt
-        val mHeroImage: ImageView = mView.heroImage
-        val mVictory: TextView = mView.victory
-        val mStateIcon: ImageView = mView.stateIcon
+    inner class ItemViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val matchType: TextView = mView.matchType
+        val matchTime: TextView = mView.matchTime
+        val backupButton: Button = mView.backupButton
+        val successPrompt: TextView = mView.successPrompt
+        val heroImage: ImageView = mView.heroImage
+        val victory: TextView = mView.victory
+        val stateIcon: ImageView = mView.stateIcon
+    }
+
+    inner class MenuViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val accountName: TextView = mView.accountName
+        val switchAccount: Button = mView.switchAccount
+        val inGameRecord: TextView = mView.inGameRecord
+        val startBackup: Button = mView.startBackup
+        val alreadyBackupRecord: TextView = mView.alreadyBackupRecord
+        val restoreBackup: Button = mView.restoreBackup
+
     }
 }
