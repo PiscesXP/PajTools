1caf017532c6ec36335ad0f2391d999b7fe44445
Zhenxi <PiscesXP@outlook.com>
2019-05-31 17:31:44 +0800

    添加详情fragment.

--- app/src/main/java/xyz/piscesxp/pajtools/MainActivity.kt
+++ app/src/main/java/xyz/piscesxp/pajtools/MainActivity.kt
@@ -1,24 +1,16 @@
 package xyz.piscesxp.pajtools
 
-import android.Manifest
-import android.content.Context
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.gson.Gson
 import xyz.piscesxp.pajtools.data.HeroImage
 import xyz.piscesxp.pajtools.data.record.RecordData
 import xyz.piscesxp.pajtools.ui.record.list.RecordListFragment
-import xyz.piscesxp.pajtools.utility.PermissionChecker
-import java.io.File
-import java.lang.StringBuilder
+import xyz.piscesxp.pajtools.ui.record.list.RecordListItemDetailFragment
 
-class MainActivity : AppCompatActivity(), RecordListFragment.RecordListFragmentListeners {
+class MainActivity : AppCompatActivity(), RecordListFragment.RecordListFragmentListeners,
+    RecordListItemDetailFragment.RecordListItemDetailFragmentListeners {
     companion object {
         private val TAG = MainActivity.javaClass.canonicalName
     }
@@ -60,9 +52,14 @@ class MainActivity : AppCompatActivity(), RecordListFragment.RecordListFragmentL
 
     override fun onRecordSelected(item: RecordData) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, RecordListItemDetailFragment()).commit()
     }
 
     override fun onLoadHeroImage(heroID: Int): String? {
         return HeroImage.getImageUrlByHeroID(heroID, assets)
+    }
+
+    override fun onLoadRecordData(recordData: RecordData) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
